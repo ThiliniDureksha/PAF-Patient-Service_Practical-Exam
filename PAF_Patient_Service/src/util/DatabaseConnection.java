83340136d37e9fe3d7a39672d1437677d2e8c1f3
ThiliniDureksha <dureksha97@gmail.com>
2020-05-05 11:35:26 +0530

    addd jasp and config db connection

--- PAF_Patient_Service/src/util/DatabaseConnection.java
+++ PAF_Patient_Service/src/util/DatabaseConnection.java
@@ -1,5 +1,28 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DatabaseConnection {
 
+	public Connection connect() {
+		Connection con = null;
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+		
+			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/patient_servicedb", "root", "");
+			System.out.print("Successfully connected");
+
+		} catch (Exception e) {
+
+			System.out.print("connection fail");
+			e.printStackTrace();
+			System.out.print(e);
+		}
+
+		return con;
+	}
+	
 }
